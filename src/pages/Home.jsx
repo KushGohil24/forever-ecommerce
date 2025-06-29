80a6b0caf5648882e3644eca7eb45da786e9d481
Kush Gohil <160225182+KushGohil24@users.noreply.github.com>
2025-06-29 14:37:47 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import NewsLetterbox from '../components/NewsLetterbox'
+import NewsLetterbox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
