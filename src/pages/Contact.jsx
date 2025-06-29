97c97d7a1ca493d74c9b92a37a08f0582f8b0b23
Kush Gohil <160225182+KushGohil24@users.noreply.github.com>
2025-06-29 14:39:36 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/frontend_assets/assets";
-import NewsLetterbox from "../components/NewsLetterbox";
+import NewsLetterbox from "../components/NewsletterBox";
 
 const Contact = () => {
   return (
